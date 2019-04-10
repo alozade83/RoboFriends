300971918c05a8149b7117990b95ec6903aa5bb0
Nikolay Kosev <n_kosev@outlook.com>
2019-04-10 13:06:45 -0400

    adding a project

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Leanne Graham',
+		username: 'Bret',
+		email: 'leanne@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Erwin Howell',
+		username: 'Antonnette',
+		email: 'erwin@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Clement Bauch',
+		username: 'Samantha',
+		email: 'clement@gmail.com'
+	},
+	{
+		id: 4,
+		name: 'Patricia Lebsack',
+		username: 'Karianne',
+		email: 'patricia@gmail.com'
+	},
+	{
+		id: 5,
+		name: 'Chealsy Ditrich',
+		username: 'Kamren',
+		email: 'chealsy@gmail.com'
+	},
+	{
+		id: 6,
+		name: 'Dennis Schults',
+		username: 'Leopoldo',
+		email: 'dennis@gmail.com'
+	},
+	{
+		id: 7,
+		name: 'Kurtis Weiss',
+		username: 'Elwin',
+		email: 'kurtis@gmail.com'
+	},
+	{
+		id: 8,
+		name: 'Nick Runolf',
+		username: 'Max',
+		email: 'nick@gmail.com'
+	},
+	{
+		id: 9,
+		name: 'Glenn Reichers',
+		username: 'Delphine',
+		email: 'glenn@gmail.com'
+	},
+	{
+		id: 10,
+		name: 'Clementina DuBuque',
+		username: 'Worich',
+		email: 'clementina@gmail.com'
+	},
+	
+]
